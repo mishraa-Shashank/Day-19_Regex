7ec84176699395a15bf1a6258a7b27cdf180e4e8
Shashank <mishra.shashankofficial06@gmail.com>
2022-12-27 23:58:18 +0530

    Added: Welcome message

---
+++ src/com/day19assignment/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.day19assignment;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("User Registration System needs to ensure all validations are in place during the User Entry.");
+    }
+}
