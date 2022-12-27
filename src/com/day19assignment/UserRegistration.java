d0f08d11964475b871326d89089be715586e3b59
Shashank <mishra.shashankofficial06@gmail.com>
2022-12-28 00:13:01 +0530

    Added: Regex pattern for first name

--- src/com/day19assignment/UserRegistration.java
+++ src/com/day19assignment/UserRegistration.java
@@ -1,8 +1,25 @@
 package com.day19assignment;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    static boolean validate(String pattern, String input) {
+        return Pattern.compile(pattern).matcher(input).matches();
+    }
 
     public static void main(String[] args) {
         System.out.println("User Registration System needs to ensure all validations are in place during the User Entry.");
+
+        // UseCase-1 Valid first name
+        String firstNameRegex = "^[A-Z]{1}[a-z]{3,}$";
+        System.out.println("Enter your first name:");
+        String firstName = new Scanner(System.in).next();
+
+        boolean isMatched = validate(firstNameRegex, firstName);
+        if (isMatched)
+            System.out.println("Valid first name");
+        else
+            System.out.println("Invalid first name");
     }
 }
